fc5d7976e9a691fcd1e149c9d7d9ce6ddb360509
Brianna Mori <brianna.mori@gmail.com>
2016-02-29 17:57:04 -0800

    Loads initial File
    We avoided the credentials file.

---
+++ spot/includes/config.php
@@ -0,0 +1,32 @@
+<?php
+/*
+config.php
+
+Provides a place to put all configuration info for our app 
+
+
+*/
+
+//THIS_PAGE is the name of the current page
+define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
+
+// THIS_PAGE;
+//die;
+
+switch(THIS_PAGE)
+{
+    case "index.php":
+        $title = "Home page title";
+        $pageID = "Home Page";
+        break;
+        
+    case "template.php":
+        $title = "Template page title";
+        $pageID = "Template Page";
+        break;
+        
+    default: 
+        $title = THIS_PAGE;
+        $pageID = "";
+        
+}
